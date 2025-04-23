a959295b467fc5e45d795bf6b31e65476002b4bd
Fabio Siqueira <fabio.siqueira@picpay.com>
2025-04-23 14:49:37 -0300

    fix: test execution

---
+++ src/test/java/com/fiap/challenge/fast_food_catalog/ApplicationTests.java
@@ -0,0 +1,36 @@
+package com.fiap.challenge.fast_food_catalog;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.port = port;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    void shouldStartupApplicationCorrectly() {
+        given()
+                .when()
+                .get("/actuator/health")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("status", equalTo("UP"));
+    }
+}
